f38dbfcf7e74b12a39ecb6b7707734e48e8dbf58
Harsh Kashyap <harshkashyap307@gmail.com>
2020-12-21 16:07:33 +0530

    Start styling Chat component

---
+++ whatsapp-clone-app/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBzcjXS0lCLlKxowM8S5wIo_2UNza8ic4Y",
+    authDomain: "whatsapp-clone-27a86.firebaseapp.com",
+    projectId: "whatsapp-clone-27a86",
+    storageBucket: "whatsapp-clone-27a86.appspot.com",
+    messagingSenderId: "271187519696",
+    appId: "1:271187519696:web:5c917d64e01a804ffa56d5",
+    measurementId: "G-EE1PLDESTE"
+  };
